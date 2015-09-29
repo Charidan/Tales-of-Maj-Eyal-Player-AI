52b9d21c58275f75f98862f9c224b8a6728cf7f5
Charidan <twilly0@gmail.com>
2015-09-29 15:01:32 -0700

    Patch v1.1 added turn limit waits when can't move (doesn't work right) cancels in wilderness (world map)

--- init.lua
+++ init.lua
@@ -25,8 +25,20 @@ for_module = "tome"
 version = {1,3,1}
 weight = 100
 author = { "Charidan (twilly0@gmail.com)" }
-description = [[Adds a keybind to toggle the new player AI. Set to Alt+F1 by default.
-This player AI rests, auto-explores, and then attacks enemies.]]
+description = [[Adds a keybind to activate the new player AI. Set to Alt+F1 by default.
+This player AI rests, auto-explores, and then attacks enemies.
+It will clear an entire floor or stop when it hits 1/4 health in the presence of enemies.
+Future versions may add configurable exit conditions and talent use.
+
+This AI has undefined behavior in the Sandworm Lair, use it there at your own risk.
+
+Note for Developers: This addon superwrites Player:act(), so if your addon also touches Player:act() change the load order so that the Player AI addon loads last.
+
+PATCH NOTES:
+ - Much fewer "hang" situations where the AI stops acting, but resumes control the next turn. A few persist, seemingly related to either enemies in previously unseen tiles or trying to path through walls.
+ - AI disables itself on the worldmap to prevent unstoppable exploration
+ - Added a 1000 turn limit to AI execution in case it gets stuck in a loop somehow.
+]]
 tags = { 'keybind', 'options', 'playerai' }
 hooks = true
 overload = true
--- superload/mod/class/Player.lua
+++ superload/mod/class/Player.lua
@@ -27,26 +27,60 @@ local PlayerExplore = require "mod.class.interface.PlayerExplore"
 
 local _M = loadPrevious(...)
 
-_M.ai_active = false
-
-local function aiStop()
+local aiTurnCount = 0
+local function aiStop(msg)
     _M.ai_active = false
+    _M.player_ai_resting = false
+    aiTurnCount = 0
+    if msg then game.log(msg) else game.log("#LIGHT_RED#AI Stopping!") end
 end
 
-local function spotHostiles(self, ...)
-	local esp_explore = select(1, ...)
+local function getDirNum(src, dst)
+    local dx = dst.x - src.x
+    if dx ~= 0 then dx = dx/dx end
+    local dy = dst.y - src.y
+    if dy ~= 0 then dy = dy/dy end
+    return util.coordToDir(dx, dy)
+end
+
+local function spotHostiles(self, actors_only)
 	local seen = {}
 	if not self.x then return seen end
 
-	-- Check for visible monsters.
+	-- Check for visible monsters, only see LOS actors, so telepathy wont prevent resting
 	core.fov.calc_circle(self.x, self.y, game.level.map.w, game.level.map.h, self.sight or 10, function(_, x, y) return game.level.map:opaque(x, y) end, function(_, x, y)
 		local actor = game.level.map(x, y, game.level.map.ACTOR)
 		if actor and self:reactionToward(actor) < 0 and self:canSee(actor) and game.level.map.seens(x, y) then
-			seen[#seen + 1] = {x=x,y=y,actor=actor}
-      --game.log("distance: "..core.fov.distance(x, y, self.x, self.y))
+			seen[#seen + 1] = {x=x,y=y,actor=actor, entity=actor, name=actor.name}
 		end
 	end, nil)
-	
+
+	if not actors_only then
+		-- Check for projectiles in line of sight
+		core.fov.calc_circle(self.x, self.y, game.level.map.w, game.level.map.h, self.sight or 10, function(_, x, y) return game.level.map:opaque(x, y) end, function(_, x, y)
+			local proj = game.level.map(x, y, game.level.map.PROJECTILE)
+			if not proj or not game.level.map.seens(x, y) then return end
+
+			-- trust ourselves but not our friends
+			if proj.src and self == proj.src then return end
+			local sx, sy = proj.start_x, proj.start_y
+			local tx, ty
+
+			-- Bresenham is too so check if we're anywhere near the mathematical line of flight
+			if type(proj.project) == "table" then
+				tx, ty = proj.project.def.x, proj.project.def.y
+			elseif proj.homing then
+				tx, ty = proj.homing.target.x, proj.homing.target.y
+			end
+			if tx and ty then
+				local dist_to_line = math.abs((self.x - sx) * (ty - sy) - (self.y - sy) * (tx - sx)) / core.fov.distance(sx, sy, tx, ty)
+				local our_way = ((self.x - x) * (tx - x) + (self.y - y) * (ty - y)) > 0
+				if our_way and dist_to_line < 1.0 then
+					seen[#seen+1] = {x=x, y=y, projectile=proj, entity=proj, name=(proj.getName and proj:getName()) or proj.name}
+				end
+			end
+		end, nil)
+	end
 	return seen
 end
 
@@ -66,21 +100,14 @@ end
 local function checkLowHealth()
     local enemy = getNearestHostile()
     if enemy ~= nil and game.player.life < game.player.max_life/4 then
--- TODO: 
-        -- use healing talents
-        aiStop()
         local dir = game.level.map:compassDirection(enemy.x - game.player.x, enemy.y - game.player.y)
-        game.log("#LIGHT_RED#(checkLowHealth) Low Health! Incoming cancel message!")
 		return true, ("#RED#AI cancelled for low health while hostile spotted to the %s (%s%s)"):format(dir or "???", enemy.name, game.level.map:isOnScreen(enemy.x, enemy.y) and "" or " - offscreen")
     end
-    game.log("#LIGHT_GREEN#(checkLowHealth) Health fine OR no enemy")
 end
 
 local function player_ai_rest() end
 
-local function player_ai_after_rest()
-    game.log("#GREEN#Start of Player AI After Rest! #GOLD#Player AI is: %s", _M.ai_active and "#LIGHT_GREEN#enabled" or "#LIGHT_RED#disabled")
-    
+local function player_ai_after_rest() 
 --TODO rework so we don't check for hostiles twice
     local ret, msg = checkLowHealth()
     if ret then return game.log(msg) end
@@ -88,52 +115,68 @@ local function player_ai_after_rest()
     local target = getNearestHostile()
     if target == nil
     then
-        game.log("#RED#No Target! Auto-Exploring!")
         --If we stopped autoexploring on a level exit, stop the AI
         local terrain = game.level.map(game.player.x, game.player.y, Map.TERRAIN)
         if game.player:autoExplore() and terrain.change_level then
-            aiStop()
+            aiStop("#LIGHT_RED#AI stopping: level change found")
         end
     else
-        game.log("#RED#Target found! Approaching!")
+        local move_success = false
+        
         local a = Astar.new(game.level.map, game.player)
         local path = a:calc(game.player.x, game.player.y, target.x, target.y)
+        
         if not path then
-            game.log("#RED#Path not found, trying beeline")
-            game.log("#GOLD#player x = #GREEND#%d #GOLD#player y = #GREEND#%d #GOLD#target x = #GREEND#%d #GOLD#target y = %d",game.player.x, game.player.y, target.x, target.y)
-            --game.player:moveDir(util.coordToDir(game.player.x, game.player.y, target.x, target.y))
+            --game.log("#RED#Path not found, trying beeline")
+            local dir = getDirNum(game.player, target)
+            move_success = game.player:attackOrMoveDir(dir)
         else
+            --game.log("#GREEN#move via path")
             local moved = game.player:move(path[1].x, path[1].y)
             if not moved then
-                game.log("#RED#Normal movement failed, trying beeline")
-                game.log("#GOLD#player x = #GREEND#%d #GOLD#player y = #GREEND#%d #GOLD#target x = #GREEND#%d #GOLD#target y = %d",game.player.x, game.player.y, target.x, target.y)
-                --game.player:moveDir(util.coordToDir(game.player.x, game.player.y, target.x, target.y))
+                --game.log("#RED#Normal movement failed, trying beeline")
+                local enemies = spotHostiles(game.player)
+                for index,enemy in pairs(enemies) do
+                    local dir = getDirNum(game.player, target)
+                    if game.player:attackOrMoveDir(dir) then
+                         move_success = true
+                        break
+                    end
+                end
+            else
+                move_success = true
             end
         end
+        if not move_success then
+            game.log("#GOLD#Waiting a turn!")
+            game.player:attackOrMoveDir(5)
+        end
     end
-    
-    game.log("#GREEN#End of Player AI After Rest! #GOLD#Player AI is: %s", _M.ai_active and "#LIGHT_GREEN#enabled" or "#LIGHT_RED#disabled")
 end
 
 local function player_ai_rest()
-    game.log("#GOLD#Player AI Rest! #GOLD#Player AI is: %s", _M.ai_active and "#LIGHT_GREEN#enabled" or "#LIGHT_RED#disabled")
+    _M.player_ai_resting = true
     game.player:restInit(nil,nil,nil,nil,player_ai_after_rest)
-    game.log("#GREEN#End of Player AI Rest!")
 end
 
 local function player_ai_act()
     local ret, msg = checkLowHealth()
     if ret then 
-        game.log("#LIGHT_RED#(ai_act) Low Health! Incoming cancel message!")
-        aiStop()
-        return game.log(msg)
+        aiStop(msg)
+        return
     end
     
+    _M.player_ai_resting = true
     game.player:restInit(nil,nil,nil,nil,player_ai_rest)
 end
 
 function _M:player_ai_start()
-    game.log("#GOLD#Player AI Start! #GOLD#Player AI is: %s", _M.ai_active and "#LIGHT_GREEN#enabled" or "#LIGHT_RED#disabled")
+    if _M.ai_active == true then
+        return aiStop("#GOLD#Disabling Player AI!")
+    end
+    if game.zone.wilderness then
+        return aiStop("#RED#Player AI cannot be used in the wilderness!")
+    end
     _M.ai_active = true
     --dialog = Dialog:simplePopup("AI active!", "The AI is clearing the floor for you. Press any key to regain control...", function()
     --    aiStop()
@@ -144,10 +187,21 @@ end
 
 local old_act = _M.act
 function _M:act()
-    old_act(game.player)
-    if (not game.pause) and (not game.player.resting) and _M.ai_active then
+    local ret = old_act(game.player)
+    if (not game.player.running) and (not game.player.resting) and _M.ai_active then
+        if game.zone.wilderness then
+            aiStop("#RED#Player AI cancelled by wilderness zone!")
+            return ret
+        end
+        aiTurnCount = aiTurnCount + 1
         player_ai_act()
+        local did_ai = true
     end
+    if aiTurnCount > 1000 then
+        aiStop("#LIGHT_RED#AI Disabled. AI acted for 1000 turns. Did it get stuck?")
+    end
+    --if (not did_ai) and (not game.player.player_ai_resting) and (not game.player.running) and (not game.player.resting) then aiStop() end
+    return ret
 end
 
 return _M
